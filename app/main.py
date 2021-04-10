b95f04e48bcb9ada9f0c7bbc7479fc66ecb4e4a5
Rawan <rawanhawangledt@gmail.com>
2021-04-10 10:45:04 -0300

    Validating the FastAPI test module
    New "/ ping" route to test  the route system is functional

--- app/main.py
+++ app/main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Access /docs for API documentation"}
+
+
+@app.get(
+    "/ping",
+    name="Ping Pong Test",
+    description="""ping pong test to verify the functioning
+     of the system routes""",
+)
+async def pong():
+    return {"ping": "pong!"}
