48af09126bdbf7fdae5436f403a00ee498f1fae3
Rawan <rawanhawangledt@gmail.com>
2021-04-12 00:32:17 -0300

    Adds user routes, configuring the CORS settings
    User routes can be checked in / docs in the Swagger interface.
    
    The CORs settings are set, the integration tests with the frontend are already possible

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
-from core.routers import repository
-
+from core.routers import auth, repository, tag
+from core.models.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+# from core.models.table import Auth
 
 app = FastAPI()
 
+app.include_router(auth.router)
 app.include_router(repository.router)
+app.include_router(tag.router)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
